802f2dbc7964fa0285b40876e7e6a8834489038f
ymj20010317-afk <ymj20010317@gmail.com>
2025-11-06 23:44:18 +0900

    Delete 예제 02-02.c

--- 예제 02-02.c
+++
@@ -1,9 +0,0 @@
-﻿#include <stdio.h>  //stdio  = standard inputoutput(표준입출력)의 줄임말
-
-int main(void)
-{
-	printf("Be happy");  //문자열 Be happy 출력 
-	printf("My friend");  //문자열 My friend 출력
-
-	return 0;
-}
