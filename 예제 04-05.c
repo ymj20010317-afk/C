b78110e05ce9534fdf568946e1c98f45dd47c694
ymj20010317-afk <ymj20010317@gmail.com>
2025-11-25 01:34:02 +0900

    Add files via upload

---
+++ 예제 04-05.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>	
+
+int main(void)
+{
+	int a = 10, b = 20, c = 10;
+	int res;
+
+	res = (a > b);
+	printf("a > b : %d\n", res);
+
+	res = (a >= b);
+	printf("a >= b : %d\n", res);
+
+	res = (a < b);
+	printf("a < b : %d\n", res);
+
+	res = (a <= b);
+	printf("a <= b : %d\n", res);
+
+	res = (a <= c);
+	printf("a <= b : %d\n", res);
+
+	res = (a == b);
+	printf("a == b : %d\n", res);
+
+	res = (a != b);
+	printf("a != b : %d\n", res);
+
+	return 0;
+}
